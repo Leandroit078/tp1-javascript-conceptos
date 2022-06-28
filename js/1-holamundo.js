454d4be6fe627a4ebfd523ad3ed4fd05fd56fc41
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-06-28 18:04:05 -0300

    agregamos mensajes por diferentes pantallas

---
+++ js/1-holamundo.js
@@ -0,0 +1,14 @@
+//esto es un comentario
+
+/*esto es un comentario
+ de multiples lineas */
+
+ //mostrar un mensaje en la pantalla
+
+ document.write("<h1>hola comision 6i</h1>");
+
+ //mostrar un mensaje por consola
+ console.log("prueba de consola")
+
+ //mostrar una ventana emergente
+ alert("desde una ventana emergente")
