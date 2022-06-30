1250042ba56dcdb96185041c603eb9d54963d8e8
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-06-30 17:52:24 -0300

    ejercicios del tp1

--- js/1-holamundo.js
+++ js/1-holamundo.js
@@ -13,4 +13,23 @@
  //mostrar una ventana emergente
  //alert("desde una ventana emergente")
  //sumemos dos valores
- document.write("Resultado: "+ (12 + 5))
+ document.write("Resultado: "+ (12 + 5));
+
+ document.write('<br>Esto es una prueba');
+
+ //para crear una variable selecciono (var - let - const)
+
+ let nombre = prompt('Ingrese su nombre');
+ console.log(nombre)
+ 
+ document.write('<br>Hola ' + nombre);
+
+ document.write('<p> Hola '+ nombre +'</p>');
+
+ //ejemplo de constantes
+
+ const url =' https://campus.rollingcodeschool.com/'
+
+ document.write(url)
+
+ url = 'otro link'
