ed6acbfc2a98f6d4bb051cc50a6a72d1dbc57da6
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-05 18:48:23 -0300

    hicimos un archivo donde explica  bucles con la funcion while

---
+++ js/3-bucles.js
@@ -0,0 +1,15 @@
+/*
+while(condicion logica){
+   todas las lineas de codigo que quiero que repita el programa
+   cambiar la condicion logica
+}
+
+*/
+
+let mates = 1;
+
+while(mates <= 20){
+    document.write("<br>preparar el mate y pasarlo al siguiente compañero, mate cebado n: "+ mates);
+    mates= mates+1; // esto es igual a cantidas++ // cantidad+= cantidad, esto es que se sume acumulativamente
+}
+
