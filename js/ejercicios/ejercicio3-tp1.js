32090cadf4baa455530510a56e681ece005746f0
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-04 22:58:49 -0300

    hice hasta el ejercicio 5

---
+++ js/ejercicios/ejercicio3-tp1.js
@@ -0,0 +1,2 @@
+document.write("<br> 3 + 5<br>");
+document.write("Resultado: "+ (3 + 5));
---
+++ js/ejercicios/ejercicio4-tp1.js
@@ -0,0 +1,4 @@
+let nombre = prompt('Ingrese su nombre');
+ console.log(nombre)
+ 
+ document.write('<br>Hola ' + nombre);
