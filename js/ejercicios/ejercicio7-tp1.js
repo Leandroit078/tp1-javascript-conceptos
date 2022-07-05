ca5338f445936a45f0dd1db47d54bffd64370d0e
Leandro Villafañe <leandro.villafane.utn@gmail.com>
2022-07-05 16:55:20 -0300

    ejercicio 7 modificado sacado de internet

--- js/ejercicios/ejercicio7-tp1.js
+++ js/ejercicios/ejercicio7-tp1.js
@@ -6,21 +6,11 @@ console.log(numero1);
 console.log(numero2);
 console.log(numero3);
 
-if(numero1 > numero2) { 
+if ( num1 > num2 && num1 > num3) {
     document.write('El primer numero es el mayor ' + numero1);
-}
-else if (numero1 < numero2) {
+   }else if (num 2 > num3){
     document.write('el segundo valor es el mayor ' + numero2); 
-}
- else if (numero1 > numero3) {
-    document.write('El primer numero es el mayor ' + numero1); 
-}
-else if (numero1 < numero3) {
+   }else{
     document.write('El tercer numero es el mayor ' + numero3);
-}
-else if (numero2 > numero3) {
-    document.write('El segundo numero es el mayor ' + numero2);
-}
-else (numero2 < numero3) {
-    document.write('El tercer numero es el mayor ' + numero3);
-}
+   }
+
